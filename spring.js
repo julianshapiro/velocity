6e9800864b7e040961ee13205f611e0efa87d9bd
Yehonatan Daniv <maggotfish@gmail.com>
2014-05-21 00:17:12 +0300

    Removed caching and replaced with returning a closure that contains the calculated path; Refactored springRK4 to take only a single options object argument; Changed default tension to 600 and friction to 20

--- spring.js
+++ spring.js
@@ -1,73 +1,68 @@
-var tolerance = 1 / 10000,
-    actual_duration_store = {};
-
-function spring (percentComplete, duration, tension, friction) {
+function springRK4 (options) {
     var initState = {
             x       : -1,
             v       : 0,
-            tension : tension || 500,
-            friction: friction || 10
+            tension : null,
+            friction: null
         },
-        path = [initState.x + 1],
-    // the framer implementation uses seconds for animation loop
-    // see: https://github.com/koenbok/Framer/blob/master/framer/Utils.coffee#L61
-        dt = 16 / 1000, // 60 FPS
+        path = [0],
         time_lapsed = 0,
-        _continueAnimation = true,
-        actual_duration, last_state;
+        tolerance = 1 / 10000,
+        // default time delta
+        /* the framer implementation uses seconds for animation loop
+           see: https://github.com/koenbok/Framer/blob/master/framer/Utils.coffee#L61 */
+        DT = 16 / 1000, // 60 FPS
+        have_duration, duration, dt, last_state;
 
-    // since we're not saving any state for now
-    // we'll calculate the actual time it takes for this animation with given conditions
-    if ( duration != null ) {
-        var cache_key = generateCacheKey(duration, initState.tension, initState.friction);
-        // check cache
-        actual_duration = cache(cache_key);
-        if ( actual_duration == null ) {
-            // running simulation without duration
-            actual_duration = spring(1, null, tension, friction);
-            // cache it
-            cache(cache_key, actual_duration);
-        }
-        // adjusting time delta accordingly
-        dt = (actual_duration / duration) * dt;
-        // allow the actual simulation to run
-        _continueAnimation = true;
+    // this is a pre-computation run
+    duration = options.duration;
+    options.tension = options.tension || 600;
+    options.friction = options.friction || 20;
+
+    initState.tension = options.tension;
+    initState.friction = options.friction;
+
+    have_duration = duration != null;
+
+    // we need to calculate the actual time it takes for this animation with given conditions
+    if ( have_duration ) {
+        options.duration = null;
+        // running simulation without duration
+        time_lapsed = springRK4(options);
+        // compute adjusted time delta
+        dt = time_lapsed / duration * DT;
+    }
+    /* no duration, so this is when we compute the actual time for this animation with given conditions
+       and return the adjusted time delta that accounts for given duration. */
+    else {
+        dt = DT;
     }
 
-    while ( _continueAnimation ) {
-        // get a new state
-        last_state = step(dt, last_state || initState);
+    while ( true ) {
+        // get a new state - this is our actual "next/step" function
+        last_state = springIntegrateState(last_state || initState, dt);
         // store position
         path.push(1 + last_state.x);
         // elapse time
         time_lapsed += 16;
-        // check if we reached threshold of change
-        _continueAnimation = again(last_state.x, last_state.v);
+        // check if we reached threshold of change, and break if we did
+        if ( ! (Math.abs(last_state.x) > tolerance &&
+                Math.abs(last_state.v) > tolerance) ) break;
     }
 
-    // if duration is not defined then we return the actual time that elapsed in milliseconds
-    // otherwise we return the snapshot of the position, according to percentComplete
-    return duration == null ? time_lapsed : path[(percentComplete * (path.length - 1)) | 0];
-}
-
-
-function step (delta, stateBefore) {
-    return springIntegrateState(stateBefore, delta);
-}
-
-function again (x, v) {
-    return Math.abs(x) > tolerance && Math.abs(v) > tolerance;
+    /* if duration is not defined then we return the actual time required for completing
+       this animation in given duration. */
+    /* otherwise we return a closure that holds the computed path
+       and returns a snapshot of the position, according to a given percentComplete input. */
+    return ! have_duration ?
+           time_lapsed :
+           function (percentComplete) {
+                return path[(percentComplete * (path.length - 1)) | 0];
+           };
 }
 
 function springAccelerationForState (state) {
     return -state.tension * state.x - state.friction * state.v;
-}
-
-function springEvaluateState (initialState) {
-    return {
-        dx: initialState.v,
-        dv: springAccelerationForState(initialState)
-    };
 }
 
 function springEvaluateStateWithDerivative (initialState, dt, derivative) {
@@ -77,7 +72,6 @@ function springEvaluateStateWithDerivative (initialState, dt, derivative) {
         tension : initialState.tension,
         friction: initialState.friction
     };
-
     return {
         dx: state.v,
         dv: springAccelerationForState(state)
@@ -85,32 +79,16 @@ function springEvaluateStateWithDerivative (initialState, dt, derivative) {
 }
 
 function springIntegrateState (state, dt) {
-    var a = springEvaluateState(state),
+    var a = {
+            dx: state.v,
+            dv: springAccelerationForState(state)
+        },
         b = springEvaluateStateWithDerivative(state, dt * 0.5, a),
         c = springEvaluateStateWithDerivative(state, dt * 0.5, b),
         d = springEvaluateStateWithDerivative(state, dt, c),
         dxdt = 1.0 / 6.0 * (a.dx + 2.0 * (b.dx + c.dx) + d.dx),
         dvdt = 1.0 / 6.0 * (a.dv + 2.0 * (b.dv + c.dv) + d.dv);
-
     state.x = state.x + dxdt * dt;
     state.v = state.v + dvdt * dt;
-
     return state;
-}
-
-function cache (key, value) {
-    if ( arguments.length == 1 ) {
-        if ( key in actual_duration_store ) {
-            return actual_duration_store[key];
-        }
-    }
-    else if ( arguments.length == 2 ) {
-        actual_duration_store[key] = value;
-    }
-
-    return null;
-}
-
-function generateCacheKey (duration, tension, friction) {
-    return duration.toString() + '-' + tension.toString() + '-' + friction.toString();
 }
