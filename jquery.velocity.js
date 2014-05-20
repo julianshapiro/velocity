06a3872ecf88a6a0ac7bba23efb5b3268e618002
Yehonatan Daniv <maggotfish@gmail.com>
2014-05-21 00:17:51 +0300

    Integrated springRK4 easing

--- jquery.velocity.js
+++ jquery.velocity.js
@@ -350,6 +350,106 @@ Note: The biggest cause of both codebase bloat and codepath obfuscation in Veloc
         velocity.Easings["spring"] = function(p) {
             return 1 - (Math.cos(p * 4.5 * Math.PI) * Math.exp(-p * 6));
         };
+        /* Full blown Runge-Kutta method based spring easing. */
+        /* Adapted from @koenbok/Framer's SpringRK4Animator: https://github.com/koenbok/Framer/blob/c16f69fc6cd4d4f3c6c633b0819fe8d7e890685b/framer/Animators/SpringRK4Animator.coffee */
+        velocity.Easings["springRK4"] = (function () {
+            function springRK4 (options) {
+                var initState = {
+                        x       : -1,
+                        v       : 0,
+                        tension : null,
+                        friction: null
+                    },
+                    path = [0],
+                    time_lapsed = 0,
+                    tolerance = 1 / 10000,
+                    // default time delta
+                    /* the framer implementation uses seconds for animation loop
+                       see: https://github.com/koenbok/Framer/blob/master/framer/Utils.coffee#L61 */
+                    DT = 16 / 1000, // 60 FPS
+                    have_duration, duration, dt, last_state;
+
+                // this is a pre-computation run
+                duration = options.duration;
+                options.tension = options.tension || 600;
+                options.friction = options.friction || 20;
+
+                initState.tension = options.tension;
+                initState.friction = options.friction;
+
+                have_duration = duration != null;
+
+                // we need to calculate the actual time it takes for this animation with given conditions
+                if ( have_duration ) {
+                    options.duration = null;
+                    // running simulation without duration
+                    time_lapsed = springRK4(options);
+                    // compute adjusted time delta
+                    dt = time_lapsed / duration * DT;
+                }
+                /* no duration, so this is when we compute the actual time for this animation with given conditions
+                   and return the adjusted time delta that accounts for given duration. */
+                else {
+                    dt = DT;
+                }
+
+                while ( true ) {
+                    // get a new state - this is our actual "next/step" function
+                    last_state = springIntegrateState(last_state || initState, dt);
+                    // store position
+                    path.push(1 + last_state.x);
+                    // elapse time
+                    time_lapsed += 16;
+                    // check if we reached threshold of change, and break if we did
+                    if ( ! (Math.abs(last_state.x) > tolerance &&
+                            Math.abs(last_state.v) > tolerance) ) break;
+                }
+
+                /* if duration is not defined then we return the actual time required for completing
+                   this animation in given duration. */
+                /* otherwise we return a closure that holds the computed path
+                   and returns a snapshot of the position, according to a given percentComplete input. */
+                return ! have_duration ?
+                       time_lapsed :
+                       function (percentComplete) {
+                            return path[(percentComplete * (path.length - 1)) | 0];
+                       };
+            }
+
+            function springAccelerationForState (state) {
+                return -state.tension * state.x - state.friction * state.v;
+            }
+
+            function springEvaluateStateWithDerivative (initialState, dt, derivative) {
+                var state = {
+                    x       : initialState.x + derivative.dx * dt,
+                    v       : initialState.v + derivative.dv * dt,
+                    tension : initialState.tension,
+                    friction: initialState.friction
+                };
+                return {
+                    dx: state.v,
+                    dv: springAccelerationForState(state)
+                };
+            }
+
+            function springIntegrateState (state, dt) {
+                var a = {
+                        dx: state.v,
+                        dv: springAccelerationForState(state)
+                    },
+                    b = springEvaluateStateWithDerivative(state, dt * 0.5, a),
+                    c = springEvaluateStateWithDerivative(state, dt * 0.5, b),
+                    d = springEvaluateStateWithDerivative(state, dt, c),
+                    dxdt = 1.0 / 6.0 * (a.dx + 2.0 * (b.dx + c.dx) + d.dx),
+                    dvdt = 1.0 / 6.0 * (a.dv + 2.0 * (b.dv + c.dv) + d.dv);
+                state.x = state.x + dxdt * dt;
+                state.v = state.v + dvdt * dt;
+                return state;
+            }
+
+            return springRK4;
+        }()); 
     })();
 
     /*****************
@@ -1277,7 +1377,7 @@ Note: The biggest cause of both codebase bloat and codepath obfuscation in Veloc
 
         /* Determining the appropriate easing type consists of a waterfall of logic. Since we have to trigger this at multiple points, we wrap it in a function. */
         function getEasing(value) {
-            var easing = value;
+            var easing = value, opts;
 
             /* The easing option can either be a string that references a pre-registered easing, or it can be an four-item array of integers to be converted into a bezier curve function. */
             if (typeof value === "string") {
@@ -1285,9 +1385,16 @@ Note: The biggest cause of both codebase bloat and codepath obfuscation in Veloc
                 if (!velocity.Easings[value]) {
                     easing = false;
                 }
+                else if (value == "springRK4") {
+                    easing = velocity.Easings[value]({duration: options.duration});
+                }
             } else if (isArray(value)) {
                 /* Note, if the bezier array is of an invalid format (has an incorrect argument length or contains non-numbers), generateBezier() returns false. */
                 easing = generateBezier.apply(null, value);
+            } else if (value && value.type && velocity.Easings[value.type] ) {
+                opts = value.options || {};
+                opts.duration = opts.duration || options.duration;
+                easing = velocity.Easings[value.type](opts);
             } else {
                 easing = false;
             }
@@ -1613,7 +1720,7 @@ Note: The biggest cause of both codebase bloat and codepath obfuscation in Veloc
                             if ((!isArray(valueData[1]) && /^[\d-]/.test(valueData[1])) || isFunction(valueData[1])) {
                                 startValue = valueData[1];
                             /* Two or three-item array: If the second item is a string, treat it as an easing. */
-                            } else if (typeof valueData[1] === "string" || isArray(valueData[1])) {
+                            } else if (typeof valueData[1] === "string" || isArray(valueData[1]) || (valueData[1] && typeof valueData[1] == 'object')) {
                                 easing = getEasing(valueData[1]);
 
                                 /* Don't bother validating startValue's value now since the ensuing property cycling logic inherently does that. */
