94f435c42152c1d765bc0655557841746cd3bcbc
Julian Shapiro <julian@julianshapiro.com>
2014-06-18 10:42:46 -0700

    Version bump

--- packages/velocity/velocity.ui.js
+++ packages/velocity/velocity.ui.js
@@ -4,7 +4,7 @@
 
 /*!
 * velocity.ui.js: UI effects pack for Velocity. Load this after Velocity.
-* @version 1.1.1
+* @version 1.1.2
 * @docs http://velocityjs.org/#uiPack
 * @support <=IE8: Callouts will have no effect, and transitions will simply fade in/out. IE9/Android 2.3: Most effects are fully supported, the rest fade in/out. All other browsers: Full support.
 * @license Copyright Julian Shapiro. MIT License: http://en.wikipedia.org/wiki/MIT_License
