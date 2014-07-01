7431dccb2550149c4d25e20eddb9a372fc8e643c
Julian Shapiro <julian@julianshapiro.com>
2014-07-01 13:58:14 -0700

    Version Updates

--- packages/velocity/jquery.velocity.js
+++ packages/velocity/jquery.velocity.js
@@ -4,7 +4,7 @@
 
 /*!
 * Velocity.js: Accelerated JavaScript animation.
-* @version 0.3.0
+* @version 0.4.0
 * @docs http://velocityjs.org
 * @license Copyright 2014 Julian Shapiro. MIT License: http://en.wikipedia.org/wiki/MIT_License
 */
