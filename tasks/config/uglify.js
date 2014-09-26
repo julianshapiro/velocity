c2835df46e50ce13df9a1ff1ff8af2df7191bdab
Steve Calvert <scalvert@mobify.com>
2014-09-26 10:10:12 -0700

    Updating the min file. Bumping version number

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
     return {
         options: {
+            banner: '/*! VelocityJS.org (1.1.0). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License */\n' +
+            '/*! VelocityJS.org jQuery Shim (1.0.1). (C) 2014 The jQuery Foundation. MIT @license: en.wikipedia.org/wiki/MIT_License. */\n'
         },
         build: {
             src: 'velocity.js',
