f601d531507551ebc8419e607656fc24eeb4a751
Steve Calvert <scalvert@mobify.com>
2014-09-26 09:22:13 -0700

    Adding minified file and grunt task to build

---
+++ tasks/config/uglify.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    return {
+        options: {
+        },
+        build: {
+            src: 'velocity.js',
+            dest: 'velocity.min.js'
+        }
+    };
+};
