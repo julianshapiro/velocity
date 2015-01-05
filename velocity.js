8bbefc92d5314efccefd2d5ac8c3cadb4db4f784
Julian Shapiro <julian@julianshapiro.com>
2015-01-05 15:10:39 -0800

    1.2.1

--- velocity.js
+++ velocity.js
@@ -1,4 +1,4 @@
-/*! VelocityJS.org (1.2.0). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License */
+/*! VelocityJS.org (1.2.1). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License */
 
 /*************************
    Velocity jQuery Shim
@@ -651,7 +651,7 @@ return function (global, window, document, undefined) {
         hook: null, /* Defined below. */
         /* Velocity-wide animation time remapping for testing purposes. */
         mock: false,
-        version: { major: 1, minor: 2, patch: 0 },
+        version: { major: 1, minor: 2, patch: 1 },
         /* Set to 1 or 2 (most verbose) to output debug info to console. */
         debug: false
     };
