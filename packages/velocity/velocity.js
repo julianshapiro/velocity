a20e0f0cd355b1ab0cd2800f11507b9d542e768b
Ryc O'Chet <rycochet@rycochet.com>
2017-03-18 11:57:21 +0000

    Release 1.5.0 IE9 passes all tests, IE8 still has issues

--- packages/velocity/velocity.js
+++ packages/velocity/velocity.js
@@ -1,4 +1,4 @@
-/*! VelocityJS.org (1.4.3). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License */
+/*! VelocityJS.org (1.5.0). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License */
 
 /*************************
  Velocity jQuery Shim
@@ -777,7 +777,7 @@
 			hook: null, /* Defined below. */
 			/* Velocity-wide animation time remapping for testing purposes. */
 			mock: false,
-			version: {major: 1, minor: 4, patch: 3},
+			version: {major: 1, minor: 5, patch: 0},
 			/* Set to 1 or 2 (most verbose) to output debug info to console. */
 			debug: false,
 			/* Use rAF high resolution timestamp when available */
