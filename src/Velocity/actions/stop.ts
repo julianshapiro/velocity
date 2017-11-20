c8cb44f2dacda14d7d5ea885e3e209ce6ee4a0c6
Duskfall <cyber_andrew6@hotmail.com>
2017-11-20 17:27:19 +0200

    WIP: stop functionality refactoring

--- src/Velocity/actions/stop.ts
+++ src/Velocity/actions/stop.ts
@@ -10,6 +10,31 @@
 namespace VelocityStatic {
 
 	/**
+	 * Check if an animation should be paused / resumed.
+	 */
+	function checkAnimationShouldBeStopped(animation: AnimationCall, queueName: false | string, defaultQueue: false | string, isStopped: boolean) {
+		if (queueName === undefined || (queueName !== undefined && queueName === getValue(animation.queue, animation.options.queue, defaultQueue))) {
+			/* Check that this call was applied to the target element. */
+			/* Make sure it can't be delayed */ // TODO do we need this?
+			animation.started = true;
+			/* Remove the queue so this can't trigger any newly added animations when it finishes */
+			animation.options.queue = false;
+
+			/* Since "reverse" uses cached start values (the previous call's endValues), these values must be
+             changed to reflect the final value that the elements were actually tweened to. */
+			/* Note: If only queue:false animations are currently running on an element, it won't have a tweensContainer
+             object. Also, queue:false animations can't be reversed. */
+			animation.timeStart = -1;
+
+			/* Prematurely call completeCall() on each matched active call. Pass an additional flag for "stop" to indicate
+             that the complete callback and display:none setting should be skipped since we're completing prematurely. */
+			completeCall(animation, isStopped)
+			dequeue(animation.element, queueName)
+		}
+	}
+
+
+	/**
 	 * When the stop action is triggered, the elements' currently active call is immediately stopped. The active call might have
 	 * been applied to multiple elements, in which case all of the call's elements will be stopped. When an element
 	 * is stopped, the next item in its animation queue is immediately triggered.
@@ -17,126 +42,36 @@ namespace VelocityStatic {
 	 * or a custom queue string can be passed in.
 	 * Note: The stop command runs prior to Velocity's Queueing phase since its behavior is intended to take effect *immediately*,
 	 * regardless of the element's current queue state.
-	 * @param {HTMLorSVGElement[]} elements The collection of HTML or SVG elements
-	 * @param {StrictVelocityOptions} The strict Velocity options
-	 * @param {Promise<HTMLorSVGElement[]>} An optional promise if the user uses promises
-	 * @param {(value?: (HTMLorSVGElement[] | VelocityResult)) => void} resolver The resolve method of the promise
+	 * @param {any[]} args
+	 * @param {VelocityResult} elements
+	 * @param {VelocityPromise} promiseHandler
+	 * @param {string} action
 	 */
-	function stop(args: any[], elements: HTMLorSVGElement[] | VelocityResult, promiseHandler?: VelocityPromise, action?: string): void {
-		let callsToStop: AnimationCall[] = [],
-			/* Iterate through every active call. */
-			queueName = args[0] === undefined ? undefined : validateQueue(args[0]),
+	function stop(args: any[], elements: VelocityResult, promiseHandler?: VelocityPromise, action?: string): void {
+		let queueName = args[0] === undefined ? undefined : validateQueue(args[0]),
 			activeCall: AnimationCall,
 			defaultQueue = defaults.queue
 
 		if (isVelocityResult(elements) && elements.velocity.animations) {
 			for (let i = 0, animations = elements.velocity.animations; i < animations.length; i++) {
-				//checkAnimation(animations[i], queueName, defaultQueue, isPaused);
+				checkAnimationShouldBeStopped(animations[i], queueName, defaultQueue, action === "stop");
 			}
 		} else {
 			activeCall = State.first;
 			while (activeCall) {
 				if (!elements || _inArray.call(elements, activeCall.element)) {
-
-
-					/* Check that this call was applied to the target element. */
-					/* Make sure it can't be delayed */
-					activeCall.started = true;
-					/* Remove the queue so this can't trigger any newly added animations when it finishes */
-					activeCall.queue = false;
-					/* Optionally clear the remaining queued calls. If we're doing "finishAll" this won't find anything,
-                     due to the queue-clearing above. */
-					let animation: AnimationCall;
-
-					/* Iterate through the items in the element's queue. */
-					while ((animation = dequeue(activeCall.element, queueName, true))) {
-						let options = animation.options,
-							resolver = options._resolver;
-
-						if (resolver) {
-							resolver(animation.elements as any); // TODO: This is wrong
-							delete options._resolver;
-						}
-					}
-
-					/* Since "reverse" uses cached start values (the previous call's endValues), these values must be
-                     changed to reflect the final value that the elements were actually tweened to. */
-					/* Note: If only queue:false animations are currently running on an element, it won't have a tweensContainer
-                     object. Also, queue:false animations can't be reversed. */
-					activeCall.timeStart = -1;
-					callsToStop.push(activeCall);
-
+					checkAnimationShouldBeStopped(activeCall, queueName, defaultQueue, action === "stop");
 				}
-
 				activeCall = activeCall._next;
 			}
 		}
 
-		/*/!* Iterate through all calls and pause any that contain any of our elements *!/
-		while (activeCall) {
-			let options = activeCall.options;
-
-			/!* If true was passed in as a secondary argument, clear absolutely all calls on this element. Otherwise, only
-             clear calls associated with the relevant queue. *!/
-			/!* Call stopping logic works as follows:
-             - options === true --> stop current default queue calls (and queue:false calls), including remaining queued ones.
-             - options === undefined --> stop current queue:"" call and all queue:false calls.
-             - options === false --> stop only queue:false calls.
-             - options === "custom" --> stop current queue:"custom" call, including remaining queued ones (there is no functionality to only clear the currently-running queue:"custom" call). *!/
-
-			if (getValue(activeCall.queue, options.queue) !== queueName) {
-				activeCall = activeCall._next;
-				continue;
-			}
-
-			/!* Iterate through the calls targeted by the stop command. *!/
-			for (let i = 0, elementsLength = elements.length; i < elementsLength; i++) {
-				let element = elements[i];
-
-				/!* Check that this call was applied to the target element. *!/
-				if (element !== activeCall.element) {
-					continue;
-				}
-
-				/!* Check that this call was applied to the target element. *!/
-				/!* Make sure it can't be delayed *!/
-				activeCall.started = true;
-				/!* Remove the queue so this can't trigger any newly added animations when it finishes *!/
-				activeCall.queue = false;
-				/!* Optionally clear the remaining queued calls. If we're doing "finishAll" this won't find anything,
-                 due to the queue-clearing above. *!/
-				let animation: AnimationCall;
-
-				/!* Iterate through the items in the element's queue. *!/
-				while ((animation = dequeue(element, queueName, true))) {
-					let options = animation.options,
-						resolver = options._resolver;
-
-					if (resolver) {
-						resolver(animation.elements);
-						delete options._resolver;
-					}
-				}
-
-				/!* Since "reverse" uses cached start values (the previous call's endValues), these values must be
-                 changed to reflect the final value that the elements were actually tweened to. *!/
-				/!* Note: If only queue:false animations are currently running on an element, it won't have a tweensContainer
-                 object. Also, queue:false animations can't be reversed. *!/
-				activeCall.timeStart = -1;
-				callsToStop.push(activeCall);
-			}
-			activeCall = activeCall._next;
-		}*/
-
-		/* Prematurely call completeCall() on each matched active call. Pass an additional flag for "stop" to indicate
-         that the complete callback and display:none setting should be skipped since we're completing prematurely. */
-		callsToStop.forEach((activeCall) => {
-			completeCall(activeCall, action === "stop");
-		});
-
 		if (promiseHandler) {
-			/* Immediately resolve the promise associated with this stop call since stop runs synchronously. */
-			promiseHandler._resolver(elements as any);// TODO: This is wrong
+			if (elements && elements.then) {
+				elements.then(promiseHandler._resolver);
+			} else {
+				promiseHandler._resolver(elements);
+			}
 		}
 	}
 
